01b8a97555b4d125815988608fcd27a96738707b
henryAirhart <henry.airhart@ascte.org>
2026-04-27 10:04:09 -0500

    Create wifi.py
    contains network config info

---
+++ Weather Station/wifi.py
@@ -0,0 +1,31 @@
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor, pico_led
+import machine
+import rp2
+import sys
+
+
+ssid = 'Eng402'
+password = 'IheartCyber'
+
+
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        if rp2.bootsel_button() == 1:
+            sys.exit()
+        print('Waiting for connection...')
+        pico_led.on()
+        sleep(0.5)
+        pico_led.off()
+        sleep(0.5)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    pico_led.on()
+    return ip
